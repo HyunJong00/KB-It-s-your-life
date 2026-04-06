6dc086a567626ccd71e0d5ae26ccf42f29aa0ead
HyunJong_muticampus <hjong1010@donngguk.edu>
2026-04-06 17:47:30 +0900

    feat:day24

---
+++ 01_WEB/03_Vue/day-024-pinia/chapter13/pinia-product-app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ProductForm from './components/ProductForm.vue';
+import ProductList from './components/ProductList.vue';
+</script>
+
+<template>
+  <ProductForm />
+  <hr />
+  <ProductList />
+</template>
+
+<style scoped></style>
---
+++ 01_WEB/03_Vue/day-024-pinia/chapter13/todolist-app-router-axios-pinia/src/pages/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div class="col p-3">
+      <router-link class="btn btn-primary" to="/todos/add"
+        >할일 추가</router-link
+      >
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <ul class="ist-group">
+        <TodoItem
+          v-for="todoItem in todoList"
+          :key="todoItem.id"
+          :todoItem="todoItem"
+        />
+      </ul>
+      <span>완료된 할일: {{ doneCount }}</span>
+    </div>
+  </div>
+  <h2>Todo 목록 보기</h2>
+</template>
+
+<script setup>
+import TodoItem from '../components/TodoItem.vue';
+import { useTodoListStore } from '../store/todoList';
+import { computed } from 'vue';
+
+const todoListStore = useTodoListStore();
+const { fetchTodoList } = todoListStore;
+const doneCount = computed(() => todoListStore.doneCount);
+const todoList = computed(() => todoListStore.todoList);
+</script>
