011903c4a2d7d65793eb937ce211e6b95850952e
HyunJong_muticampus <hjong1010@donngguk.edu>
2026-04-15 17:49:19 +0900

    feat:day26

---
+++ 02_JAVA/day-026-variable/week2/src/ch04/sec04/SumFrom1To100Example2.java
@@ -0,0 +1,12 @@
+package ch04.sec04;
+
+public class SumFrom1To100Example2 {
+    public static void main(String[] args) {
+        int i = 0;
+        int sum =0;
+        while(i<=100){
+            sum += i++;
+        }
+        System.out.println("1~100 합 : "+sum);
+    }
+}
