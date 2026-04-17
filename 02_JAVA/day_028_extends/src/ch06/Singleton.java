2abae1e1e62f97c8e0b00d2846459c9a618fc9c6
HyunJong_muticampus <hjong1010@donngguk.edu>
2026-04-17 17:06:17 +0900

    feat:day28

---
+++ 02_JAVA/day_028_extends/src/ch06/Singleton.java
@@ -0,0 +1,10 @@
+package day_028_extends.src.ch06;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton(){}
+    public static Singleton getInstance(){
+        return singleton;
+    }
+}
---
+++ 02_JAVA/day_028_extends/src/get_set/Car.java
@@ -0,0 +1,30 @@
+package day_028_extends.src.get_set;
+
+public class Car {
+    private int speed;
+    private boolean stop;
+
+    public Car() {
+    }
+
+    public Car(int speed, boolean stop) {
+        this.speed = speed;
+        this.stop = stop;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+    }
+}
